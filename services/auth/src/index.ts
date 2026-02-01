4df0d63d712c6254b7bf61da40325e22e7209c6b
arakhita <arakhitaSabata635@gmail.com>
2026-02-01 19:05:06 +0530

    feat: define initial database schema for users and skills

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,10 +1,21 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import initDb from "./db/init.js";
 
 dotenv.config();
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `auth server is running on https://localhost:${process.env.PORT}`,
-  );
-});
+async function startServer() {
+  try {
+    await initDb();
+
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `🚀 Auth service running on http://localhost:${process.env.PORT}`,
+      );
+    });
+  } catch (error) {
+    process.exit(1);
+  }
+}
+
+startServer();
