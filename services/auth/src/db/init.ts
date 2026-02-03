7d430c542a2bc11631fe026c9fa74a4bbdaedb8f
arakhita <arakhitaSabata635@gmail.com>
2026-02-03 15:55:22 +0530

    refector: add isemail varify in user db

--- services/auth/src/db/init.ts
+++ services/auth/src/db/init.ts
@@ -16,6 +16,7 @@ async function initDb() {
   user_id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   email  VARCHAR(255) NOT NULL,
+  email_varified BOOLEAN DEFAULT FALSE,
   password VARCHAR(255) NOT NULL,
   phone_number VARCHAR(20),
   role user_role NOT NULL,
@@ -49,4 +50,4 @@ async function initDb() {
   }
 }
 
-export default initDb
+export default initDb;
