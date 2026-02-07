778384a996eb3fcae9d9a10d93461186690122b8
arakhita <arakhitaSabata635@gmail.com>
2026-02-07 12:42:28 +0530

    start shifting the token logic to different table

--- services/auth/src/controller/auth.ts
+++ services/auth/src/controller/auth.ts
@@ -11,7 +11,7 @@ import { AppError } from "../utils/errorClass.js";
 import { CookieOptions } from "express";
 
 type LoginRes = {
-  data: UserDTO;
+  user: UserDTO;
   accessToken: string;
 };
 const cookieOption: CookieOptions = {
@@ -19,6 +19,11 @@ const cookieOption: CookieOptions = {
   secure: process.env.NODE_ENV === "production",
   sameSite: "strict",
   maxAge: 7 * 24 * 60 * 60 * 1000,
+};
+const clearCookieOption: CookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: "strict",
 };
 export const registerUserController: controller = async (req, res, next) => {
   let createdUser = await registerUserService(req.body);
@@ -32,7 +37,7 @@ export const registerUserController: controller = async (req, res, next) => {
 
 export const loginUserController: controller = async (req, res, next) => {
   if (req.cookies["refreshToken"])
-    res.clearCookie("refreshToken", cookieOption);
+    res.clearCookie("refreshToken", clearCookieOption);
   const { userDTO, accessToken, refreshToken } = await loginUserService(
     req.body,
   );
@@ -40,7 +45,7 @@ export const loginUserController: controller = async (req, res, next) => {
   res.cookie("refreshToken", refreshToken, cookieOption);
   return sendSuccess<LoginRes>(
     res,
-    { data: userDTO, accessToken },
+    { user: userDTO, accessToken },
     "user Login successfully",
     200,
   );
@@ -54,7 +59,7 @@ export const refreshAccessTokenController: controller = async (
   const refreshToken: string | undefined = req.cookies["refreshToken"];
   if (!refreshToken)
     throw new AppError(400, "Refresh token is missing or null.");
-  res.clearCookie("refreshToken", cookieOption);
+  res.clearCookie("refreshToken", clearCookieOption);
   const { accessToken, newRefreshToken } =
     await createAccessTokenService(refreshToken);
   res.cookie("refreshToken", newRefreshToken, cookieOption);
@@ -65,6 +70,6 @@ export const logoutControler: controller = async (req, res, next) => {
   const refreshToken: string | undefined = req.cookies["refreshToken"];
   if (!refreshToken) throw new AppError(204, "User is already logout");
   const result = await logoutService(refreshToken);
-  res.clearCookie("refreshToken", cookieOption);
+  res.clearCookie("refreshToken", clearCookieOption);
   return sendSuccess<{}>(res, {}, "Logout succefully", 204);
 };
